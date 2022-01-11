d5b6c69fe20b602931ea83fec27b7ce9fa2ed0e8
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 20:35:31 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let nbre_pas = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(nbre_pas)
+})
+input.onButtonPressed(Button.AB, function () {
+    nbre_pas = 0
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake) || input.isGesture(Gesture.LogoDown)) {
+        nbre_pas += 1
+    }
+})
